c2cec2b8d38242c6ad0c0d3cafd36b78b14dfa1a
Lucas Haupt <l.haupt@einhundert-energie.de>
2024-05-02 17:27:09 +0200

    Add notice to modfilter.py callback funcs to return false to exclude a module from the graph.

--- src/modfilter.py
+++ src/modfilter.py
@@ -2,10 +2,14 @@ from typing import Callable
 import sys
 
 # example filterfunction for filtering specific module
+# return false to exclude module 
 def modfilterfunc(modname: str, parentname: str) -> bool:
         return not is_test_module(parentname)
 
+
+
 # filterfunction for filtering specific top module
+# return false to exclude module
 #def pkgfilterfunc(topmodname: str) -> bool:
 #        return True
 
