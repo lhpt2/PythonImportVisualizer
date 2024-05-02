000b5313f179b366424bf5d667e1dc86f837ea4e
Lucas Haupt <l.haupt@einhundert-energie.de>
2024-05-02 17:34:51 +0200

    Remove the `pkgfilterfunc` from modfilter module and add notice to return False to exclude a module

--- src/modfilter.py
+++ src/modfilter.py
@@ -1,7 +1,4 @@
 # example filterfunction for filtering specific module
+# return False to exclude a module
 def modfilterfunc(modname: str, parentname: str) -> bool:
-        return True
-
-# filterfunction for filtering specific top module
-def pkgfilterfunc(topmodname: str) -> bool:
         return True
