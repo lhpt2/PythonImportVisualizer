c7f7b1698eababfcbda164ed7111757da8851138
Lucas Haupt <l.haupt@einhundert-energie.de>
2024-05-02 16:55:25 +0200

    Refactor: Add filtering conditions for 100 project

--- src/modfilter.py
+++ src/modfilter.py
@@ -1,7 +1,12 @@
+from typing import Callable
+import sys
+
 # example filterfunction for filtering specific module
 def modfilterfunc(modname: str, parentname: str) -> bool:
-        return True
+        return not is_test_module(parentname):
 
 # filterfunction for filtering specific top module
-def pkgfilterfunc(topmodname: str) -> bool:
-        return True
+#def pkgfilterfunc(topmodname: str) -> bool:
+#        return True
+
+is_test_module: Callable[[str], bool] = lambda modname: '.tests' in modname
