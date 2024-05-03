1aca2ded43c235bf60385b28286c90cec25b8639
Lucas Haupt <l.haupt@einhundert-energie.de>
2024-05-03 06:13:55 +0200

    Refactor modfilter.py: Remove redundant return statement

--- src/modfilter.py
+++ src/modfilter.py
@@ -9,12 +9,8 @@ def parent_mod_filter_func(mod_dict: Dict) -> Dict:
         temp = dict(mod_dict)
         for name, _ in mod_dict.items(): 
                 if not parent_filter(name):
-                        eprint("Filter: ", name)
                         del temp[name]
         return temp 
-
-
-        return mod_dict
 
 def parent_filter(modname: str) -> bool:
         return not (is_test_module(modname) or is_logging_module(modname))
