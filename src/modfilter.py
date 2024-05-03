94ce0b14aed44fcdf2f74c62d12ec759f4b0f8f3
Lucas Haupt <l.haupt@einhundert-energie.de>
2024-05-03 06:31:58 +0200

    Add additional filters (filter django modules)

--- src/modfilter.py
+++ src/modfilter.py
@@ -13,14 +13,16 @@ def parent_mod_filter_func(mod_dict: Dict) -> Dict:
         return temp 
 
 def parent_filter(modname: str) -> bool:
-        return not (is_test_module(modname) or is_logging_module(modname))
+        return not (is_test_module(modname) or is_logging_module(modname) or is_django_module(modname))
 
 # example filterfunction for filtering specific module
 # return false to exclude module 
 def import_mod_filter_func(modname: str, parentname: str) -> bool:
-        return not (is_test_module(parentname) or is_logging_module(parentname))
+        return not (is_test_module(parentname) or is_logging_module(parentname) or is_django_module(modname))
 
 
 is_test_module: Callable[[str], bool] = lambda modname: '.tests' in modname
 
 is_logging_module: Callable[[str], bool] = lambda modname: 'logging' in modname
+
+is_django_module: Callable[[str], bool] = lambda modname: 'django' in modname
